92eb6437bcc2aa5064d2b78f3590d8ebd5812f56
shanuka2000 <115928935+shanuka2000@users.noreply.github.com>
2023-03-20 23:06:27 +0530

    Minor fix in navigation responsiveness

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Navigation.css";
-import { BsMoonStarsFill } from "react-icons/bs";
+import { BsMoonStarsFill, BsSearch, BsHeart, BsCart3 } from "react-icons/bs";
+import { IoMenu } from "react-icons/io5";
 
 function Navigation() {
   return (
     <div className="nav">
       {/* Top div */}
-        {/* Currency */}
-        {/* Welcome text */}
-        {/* Login register btns */}
+      {/* Currency */}
+      {/* Welcome text */}
+      {/* Login register btns */}
 
       <div className="nav__top">
         <p className="nav__top__left">Currency: LKR</p>
@@ -23,11 +24,37 @@ function Navigation() {
       </div>
 
       {/* Bottom div */}
-        {/* left button links home, shop */}
-        {/* logo */}
-        {/* search btn, cart, wishlist */}
-      
-      
+      {/* left button links home, shop */}
+      {/* logo */}
+      {/* search btn, cart, wishlist */}
+
+      <div className="nav__bottom">
+        <div className="nav__bottom__left">
+          <a href="#">Home</a>
+          <a href="#">Shop</a>
+          <a href="#">About</a>
+        </div>
+        <div className="nav__bottom__mid">
+          <a href="#">/ LUMOS \</a>
+        </div>
+        <div className="nav__bottom__right">
+          <div className="search">
+            <BsSearch className="icon" />
+            <p>Search</p>
+          </div>
+          <div className="wish">
+            <BsHeart className="icon" />
+            <p>Wishlist</p>
+          </div>
+          <div className="cart">
+            <BsCart3 className="icon" />
+            <p>1 item</p>
+          </div>
+          <div className="ham">
+            <IoMenu className="hamSide" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
