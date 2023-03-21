5a0a4b7805cb036d3d5b1ed3c2e9f2b1a3cf3bb3
shanuka2000 <115928935+shanuka2000@users.noreply.github.com>
2023-03-22 00:46:24 +0530

    Minor update TopSeller

--- src/components/TopSeller.jsx
+++ src/components/TopSeller.jsx
@@ -6,34 +6,34 @@ function TopSeller() {
   const data = [
     {
       itemId: 0,
-      imageUrl: "0.webp",
+      imageUrl: "src/assets/topSellers/0.webp",
       title: "Personalised Gryffindor Robe",
       price: "£4.95",
     },
     {
       itemId: 1,
-      imageUrl: "1.webp",
+      imageUrl: "src/assets/topSellers/1.webp",
       title: "Bottled Butterbeer 4-Pack",
       price: "£13.95",
     },
     {
       itemId: 2,
-      imageUrl: "2.webp",
+      imageUrl: "src/assets/topSellers/2.webp",
       title: "Exploding Bon Bons",
       price: "£8.00",
     },
     {
       itemId: 3,
-      imageUrl: "3.webp",
+      imageUrl: "src/assets/topSellers/3.webp",
       title: "Personalised Slytherin Robe",
       price: "£59.96",
     },
-    {
-      itemId: 4,
-      imageUrl: "4.webp",
-      title: "Fizzing Whizzbees Chocolate",
-      price: "£8.00",
-    },
+    // {
+    //   itemId: 4,
+    //   imageUrl: "src/assets/topSellers/4.webp",
+    //   title: "Fizzing Whizzbees Chocolate",
+    //   price: "£8.00",
+    // },
   ];
 
   return (
@@ -41,14 +41,17 @@ function TopSeller() {
       <div className="TopSeller__heading">
         <h1>Top Sellers</h1>
       </div>
-      {data.map((item) => (
-        <Item
-          id={item.itemId}
-          imgUrl={item.imageUrl}
-          title={item.title}
-          price={item.price}
-        />
-      ))}
+      <div className="TopSeller_carousel">
+        {data.map((item) => (
+          <Item
+          key={item.itemId}
+            id={item.itemId}
+            imgUrl={item.imageUrl}
+            title={item.title}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
 }
